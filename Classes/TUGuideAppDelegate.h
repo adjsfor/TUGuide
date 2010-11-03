ef8a00972c36a6ac599708dc9348e5b177dcdf49
Adrian Djokic <e0928197@student.tuwien.ac.at>
2010-11-03 18:33:19 +0100

    first commit

---
+++ Classes/TUGuideAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  TUGuideAppDelegate.h
+//  TUGuide
+//
+//  Created by Adrian Djokic on 11/3/10.
+//  Copyright 2010 Bundesrechenzentrum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TUGuideAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TUGuide
+//
+//  Created by Adrian Djokic on 11/3/10.
+//  Copyright 2010 Bundesrechenzentrum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}

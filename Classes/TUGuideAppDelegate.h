8efbbbd84ef041c9c6ab56104db9a1268812bff8
galic1987 <galic1987@gmail.com>
2010-11-26 15:39:55 +0100

    test

--- Classes/TUGuideAppDelegate.h
+++ Classes/TUGuideAppDelegate.h
@@ -4,7 +4,8 @@
 //
 //  Created by Adrian Djokic on 11/3/10.
 //  Copyright 2010 Bundesrechenzentrum. All rights reserved.
-// Das ist testing von SCM VON IVO
+// Das ist testing von SCM ivo
+// bla bla balb la
 
 #import <UIKit/UIKit.h>
 

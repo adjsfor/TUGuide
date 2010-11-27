7ec8765d6fb5f630af7d54748d9597309964a7e7
Takarashy <jakub.kolesik@chello.at>
2010-11-27 16:27:25 +0100

    Added Eatery

---
+++ Classes/Eatery.h
@@ -0,0 +1,16 @@
+//
+//  Eatery.h
+//  TUGuide
+//
+//  Created by Jakub Kolesik on 27.11.10.
+//  Copyright 2010 TU WIEN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Eatery : NSObject {
+
+}
+
+@end
---
+++ Classes/Eatery.m
@@ -0,0 +1,14 @@
+//
+//  Eatery.m
+//  TUGuide
+//
+//  Created by Jakub Kolesik on 27.11.10.
+//  Copyright 2010 TU WIEN. All rights reserved.
+//
+
+#import "Eatery.h"
+
+
+@implementation Eatery
+
+@end

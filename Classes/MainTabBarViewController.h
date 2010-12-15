05d3a687ad7e68b6d333612bed5f881cc8bc85de
galic1987 <galic1987@gmail.com>
2010-12-15 16:22:24 +0100

    new files

---
+++ Classes/MainTabBarViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainTabBarViewController.h
+//  TUGuide
+//
+//  Created by Ivo Galic on 12/15/10.
+//  Copyright 2010 Galic Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MainTabBarViewController : UITabBarController {
+	
+}
+
+@end
